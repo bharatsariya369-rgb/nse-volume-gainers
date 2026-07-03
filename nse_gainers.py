6c8a1e30ef8462e5ecce87b68090fae749e69522
bharatsariya369-rgb <bharatsariya369@gmail.com>
2026-07-03 19:52:14 +0530

    Update nse_gainers.py

--- nse_gainers.py
+++ nse_gainers.py
@@ -151,8 +151,8 @@ else:
         with open(DATA_FILE, "r") as f:
             morning_data = json.load(f)
 
-print("Morning symbols:")
-print(list(morning_data.keys()))
+        print("Morning symbols:")
+        print(list(morning_data.keys()))
 
     except Exception as e:
 
@@ -167,8 +167,8 @@ print(list(morning_data.keys()))
     for stock in evening_stocks:
         evening_lookup[stock["symbol"]] = stock
 
-print("Evening symbols:")
-print(list(evening_lookup.keys()))
+    print("Evening symbols:")
+    print(list(evening_lookup.keys()))
 
     print("Morning count:", len(morning_data))
     print("Evening count:", len(evening_lookup))
